a60fc6a9e08c9f6c7770c14bb42c0fd75635e364
Iti Debnath <itiidebnath60@gmail.com>
2025-07-07 18:46:20 +0530

    Update CategoryNav.js

--- frontend/src/components/CategoryNav.js
+++ frontend/src/components/CategoryNav.js
@@ -1,4 +1,4 @@
-// frontend/src/components/CategoryNav.js
+
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
@@ -16,7 +16,7 @@ const categories = [
 ];
 
 const CategoryNav = () => {
-    const { genre } = useParams(); // Get current genre from URL
+    const { genre } = useParams(); 
 
     return (
         <div className="category-nav">
